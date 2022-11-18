4c3eb85964225964a7cc3033a04ded003ee393de
Rowland <rowlandugiangbe@gmail.com>
2022-11-18 11:13:15 +0100

    learn

--- src/app/quality/quality.component.ts
+++ src/app/quality/quality.component.ts
@@ -14,6 +14,7 @@ export class QualityComponent implements OnInit {
   showImage: boolean = false;
   errorMessage: string = '';
 
+  // Filter
   private _listFilter: string = '';
   get listFilter(): string {
     return this._listFilter;
